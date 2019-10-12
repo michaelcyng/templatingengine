0cb8c29ea10cdcb21062148ba401919440bb1e23
michaelcyng <choyiung@yahoo.com.hk>
2019-10-13 01:00:57 +0200

    Unify all conversions to be done with ValueHandler constructor

--- include/dataTypes/ValueHandler.h
+++ include/dataTypes/ValueHandler.h
@@ -20,29 +20,24 @@ namespace templatingengine {
         ValueHandler(const std::string& value); // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
         ValueHandler(ValueBasePtr_t valuePtr); // NOLINT(google-explicit-constructor,hicpp-explicit-conversions)
 
+        // Convert from container type to ValueHandler which contains a ValueList shared pointer
+        template <typename Container,
+                std::enable_if_t<std::is_convertible<typename Container::value_type, ValueHandler>::value, int> = 0>
+        ValueHandler(const Container& valueContainer):
+            myValuePtr(std::static_pointer_cast<ValueBase>(std::make_shared<ValueList>(
+                    std::list<ValueHandler>(valueContainer.begin(), valueContainer.end())))) {
+        }
+
+        // Support for conversion from initializer list as it cannot be inferred as template argument
         template <typename T, std::enable_if_t<std::is_convertible<T, ValueHandler>::value, int> = 0 >
-        ValueHandler(const std::initializer_list<T>& valueList) {
-            std::list<ValueHandler> valueHandlerList;
-            for (const auto& value: valueList) {
-                valueHandlerList.emplace_back(value);
-            }
-            myValuePtr = std::static_pointer_cast<ValueBase>(std::make_shared<ValueList>(valueHandlerList));
+        ValueHandler(const std::initializer_list<T>& valueList):
+                myValuePtr(std::static_pointer_cast<ValueBase>(std::make_shared<ValueList>(
+                        std::list<ValueHandler>(valueList.begin(), valueList.end())))) {
         }
 
         ValueBasePtr_t&       operator->();
         const ValueBasePtr_t& operator->() const;
         ValueBase&            operator*();
-
-        template <typename Container,
-                  std::enable_if_t<std::is_convertible<typename Container::value_type, ValueHandler>::value, int> = 0>
-        ValueHandler& operator=(const Container& valueContainer) {
-            std::list<ValueHandler> valueHandlerList;
-            for (const auto& value: valueContainer) {
-                valueHandlerList.emplace_back(value);
-            }
-            myValuePtr = std::static_pointer_cast<ValueBase>(std::make_shared<ValueList>(valueHandlerList));
-            return *this;
-        }
 
         ValueHandler& operator=(const ValueHandler& value);
 
