b71efe7478e40a559f3328387c24046f6f5fd26f
Cho Yiu Ng (Michael) <choyiung@yahoo.com.hk>
2019-10-14 03:47:34 +0200

    Simplify RendererChainTest (#9)

--- tests/cpp/RendererChainTest.cpp
+++ tests/cpp/RendererChainTest.cpp
@@ -20,73 +20,45 @@ protected:
 };
 
 TEST_F(RendererChainTest, TestEmptyRendererChain) {
-    templatingengine::RendererChain testrendererChain;
+    templatingengine::RendererChain testRendererChain;
     templatingengine::ParameterSet parameterSet;
     std::stringstream ss;
-    testrendererChain.render(ss, parameterSet);
+    testRendererChain.render(ss, parameterSet);
 
     ASSERT_EQ(ss.str(), "");
 }
 
 TEST_F(RendererChainTest, TestLinearRendererChain) {
-    templatingengine::RendererChain testrendererChain;
+    templatingengine::RendererChain testRendererChain;
 
-    const std::string& testString = "This is sentence 1.";
-    templatingengine::RendererBasePtr_t testRenderer1 = std::make_shared<templatingengine::PlainTextRenderer>(testString);
-    testrendererChain.addRenderer(testRenderer1);
-
-    templatingengine::RendererBasePtr_t testRenderer2 = std::make_shared<templatingengine::PlainTextRenderer>("\n");
-    testrendererChain.addRenderer(testRenderer2);
-
-    templatingengine::RendererBasePtr_t testRenderer3 = std::make_shared<templatingengine::VariableRenderer>("sentence1");
-    testrendererChain.addRenderer(testRenderer3);
-
-    templatingengine::RendererBasePtr_t testRenderer4 = std::make_shared<templatingengine::VariableRenderer>("notExistingVar");
-    testrendererChain.addRenderer(testRenderer4);
-
-    templatingengine::RendererBasePtr_t testRenderer5 = std::make_shared<templatingengine::PlainTextRenderer>("");
-    testrendererChain.addRenderer(testRenderer5);
+    testRendererChain.addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("This is sentence 1."));
+    testRendererChain.addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("\n"));
+    testRendererChain.addRenderer(std::make_shared<templatingengine::VariableRenderer>("sentence1"));
+    testRendererChain.addRenderer(std::make_shared<templatingengine::VariableRenderer>("notExistingVar"));
+    testRendererChain.addRenderer(std::make_shared<templatingengine::PlainTextRenderer>(""));
 
     templatingengine::ParameterSet parameterSet;
     parameterSet["sentence1"] = "This is sentence 2.";
     parameterSet["sentence2"] = "Should not appear";
 
     std::stringstream ss;
-    testrendererChain.render(ss, parameterSet);
+    testRendererChain.render(ss, parameterSet);
 
     ASSERT_EQ(ss.str(), "This is sentence 1.\nThis is sentence 2.{{notExistingVar}}");
 }
 
 TEST_F(RendererChainTest, TestNestedRendererChain) {
-    templatingengine::RendererBasePtr_t testInnerRendererChain = std::make_shared<templatingengine::RendererChain>();
+    auto testInnerRendererChain = std::make_shared<templatingengine::RendererChain>();
     
-    const std::string& testString1 = "First inner plain text renderer.";
-    templatingengine::RendererBasePtr_t testInnerRenderer1 =
-            std::make_shared<templatingengine::PlainTextRenderer>(testString1);
-    std::static_pointer_cast<templatingengine::RendererChain>(testInnerRendererChain)->addRenderer(testInnerRenderer1);
-
-    templatingengine::RendererBasePtr_t testInnerRenderer2 = std::make_shared<templatingengine::VariableRenderer>("innerVar");
-    std::static_pointer_cast<templatingengine::RendererChain>(testInnerRendererChain)->addRenderer(testInnerRenderer2);
-
-    const std::string& testString2 = "Second inner plain text renderer.";
-    templatingengine::RendererBasePtr_t testInnerRenderer3 =
-            std::make_shared<templatingengine::PlainTextRenderer>(testString2);
-    std::static_pointer_cast<templatingengine::RendererChain>(testInnerRendererChain)->addRenderer(testInnerRenderer3);
+    testInnerRendererChain->addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("First inner plain text renderer."));
+    testInnerRendererChain->addRenderer(std::make_shared<templatingengine::VariableRenderer>("innerVar"));
+    testInnerRendererChain->addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("Second inner plain text renderer."));
 
     templatingengine::RendererChain testOuterRendererChain;
-
-    const std::string& testString3 = "First outer plain text renderer.";
-    templatingengine::RendererBasePtr_t testOuterRenderer1 = std::make_shared<templatingengine::PlainTextRenderer>(testString3);
-    testOuterRendererChain.addRenderer(testOuterRenderer1);
-
-    templatingengine::RendererBasePtr_t testOuterRenderer2 = std::make_shared<templatingengine::VariableRenderer>("outerVar");
-    testOuterRendererChain.addRenderer(testOuterRenderer2);
-
+    testOuterRendererChain.addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("First outer plain text renderer."));
+    testOuterRendererChain.addRenderer(std::make_shared<templatingengine::VariableRenderer>("outerVar"));
     testOuterRendererChain.addRenderer(testInnerRendererChain);
-
-    const std::string& testString4 = "Second outer plain text renderer.";
-    templatingengine::RendererBasePtr_t testOuterRenderer3 = std::make_shared<templatingengine::PlainTextRenderer>(testString4);
-    testOuterRendererChain.addRenderer(testOuterRenderer3);
+    testOuterRendererChain.addRenderer(std::make_shared<templatingengine::PlainTextRenderer>("Second outer plain text renderer."));
 
     templatingengine::ParameterSet parameterSet;
     parameterSet["innerVar"] = "Inner variable renderer.";
